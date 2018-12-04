b229e3d93890dd795f9be3d1c82dda4bc51f21d4
Linda Thelen <linda.thelen@arcor.de>
2018-12-04 12:01:47 +0100

    author search

--- find.py
+++ find.py
@@ -26,9 +26,21 @@ while i < len(a):
         j+=1
     i += 3
 
+i = 0
+while i < len(a):
+    #print(a[i].lower())
+    if search in a[i].lower():
+        print(str(j) + ": " + a[i+1] + " (" + a[i][1:] + ")")
+        list.append(a[i+2])
+        list2.append(a[i+1] + " (" + a[i][1:] + ")")
+        j+=1
+    i += 3
+
 if j == 1:
     print("Suchterm nicht gefunden")
     sys.exit()
 
-c = int(raw_input("Which title do you choose? (please give the number) "))-1
+c = int(raw_input("Which title do you choose? (please give the number, 0 for exit) "))-1
+if c == -1:
+    sys.exit()
 os.system("python gutenberg.py " + list[c] + " '" + list2[c] + "'")
